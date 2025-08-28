0e95e4bf058641d739540230c8848eb50bdff1b9
jappenzeller <jappenzeller@users.noreply.github.com>
2025-08-27 20:50:02 -0400

    WebGL Login

--- SYSTEM-client-3d/WebBuild/Build/WebBuild.loader.js
+++ SYSTEM-client-3d/WebBuild/Build/WebBuild.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 199291585,
+    wasmFileSize: 199306518,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
